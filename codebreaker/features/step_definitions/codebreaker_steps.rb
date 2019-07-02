d852e8554d6c53dc174fa08208f65fe5c9b9a7f6
Kevin Jacoby <kevin.jacoby@freeagent.com>
2019-07-02 09:01:29 +0100

    Monday 1/07/19

--- codebreaker/features/step_definitions/codebreaker_steps.rb
+++ codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,22 +1,44 @@
-Given /^I am not yet playing$/ do
-end
-
-When /^I start a new game$/ do
-  Codebreaker::Game.new.start
-end
-
-Then /^I should see "([^"]*)"$/ do |arg1|
-  pending
-end
-class Output
+class MyOutput
   def messages
     @messages ||= []
   end
+
   def puts(message)
     messages << message
   end
 end
 
-def output
-  @output ||= Output.new
+def my_output
+  @my_output ||= MyOutput.new
+end
+
+# Start game
+Given /^I am not yet playing$/ do
+  game = Codebreaker::Game.new(my_output)
+end
+
+When /^I start a new game$/ do
+  game = Codebreaker::Game.new(my_output)
+  game.start('1234')
+end
+
+Then /^I should see "([^"]*)"$/ do |message|
+  expect(my_output.messages).to include(message)
+end
+
+# Submit guess
+
+Given "the secret code is {string}" do |string|
+  @game = Codebreaker::Game.new(my_output)
+  @game.start(string)
+end
+
+When "I guess {string}" do |string|
+  #pending # Write code here that turns the phrase above into concrete actions
+  @game.guess(string)
+end
+
+Then "the mark should be {string}" do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+  expect(my_output.messages).to include(string)
 end
--- codebreaker/spec/codebreaker/game_spec.rb
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -2,14 +2,29 @@ require 'spec_helper'
 
 module Codebreaker
   describe Game do
+    let(:my_output) { double('my_output').as_null_object }
+    let(:game)      { Game.new(my_output) }
+
     describe "#start" do
       it "sends a welcome message" do
-        output = double('output')
-        game = Game.new(output)
-        expect(output).to receive(:puts).with('Welcome to Codebreaker!')
-        game.start
+        expect(my_output).to receive(:puts).with("Welcome to Codebreaker!")
+        game.start('1234')
       end
-      it "prompts for the first guess"
+
+      it "prompts for the first guess" do
+        expect(my_output).to receive(:puts).with("Enter guess:")
+        game.start('1234')
+      end
+    end
+
+    describe "#guess" do
+      context "with no matches" do
+        it "sends a mark with ''" do
+          game.start('1234')
+          expect(my_output).to receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
     end
   end
 end
